ae9b7119c7745ebb5760aace03121a0dbd919eb7
IngRonald12 <129900670+IngRonald12@users.noreply.github.com>
2023-05-28 13:16:32 -0500

    Hola mundo

--- Modulo.py
+++ Modulo.py
@@ -1,6 +1,7 @@
 import random
-from os import system; system("cls")
+import os
 import csv
+
 datos_usuario = {}
 
 def generar_codigo():
@@ -9,29 +10,79 @@ def generar_codigo():
 
 def main_menu():
     while True:
-        system("cls")
+        os.system("cls")
         print("===========================================")
-        print("     ***  SYSTEM E-CARD AGUACHICA  ***     ")
+        print("     ***  SYSTEM E-CARD AGUACHICA  ***    ")
         print("                                           ")
         print("1.   -   Comprar tarjeta")
-        print("2.   -   Registro clientes")
-        print("3.   -   Consultar tarjetas activas")
-        print("4.   -   Consultar tarjetas inactivas")
-        print("5.   -   Consultar recargas por fecha")
-        print("6.   -   Buscar usuario")
-        print("7    -   Buscar una tarjeta")
-        print("8.   -   Salir del programa")
+        print("2.   -   Recargar tarjeta")
+        print("3.   -   Registro clientes")
+        print("4.   -   Consultar tarjetas activas")
+        print("5.   -   Consultar tarjetas inactivas")
+        print("6.   -   Consultar recargas por fecha")
+        print("7.   -   Buscar usuario")
+        print("8.   -   Buscar una tarjeta")
+        
+        print("9.   -   Salir del programa")
         print("============================================")
-        opcion = int(input("Ingrese la opcion que desea utilizar: "))
+        try:
+            opcion = int(input("Ingrese la opción que desea utilizar: "))
+        except ValueError:
+            print("Ingresa una opción válida.")
+            continue
         
         if opcion == 1:
             comprar_tarjeta()
             break
+        elif opcion == 2:
+            recargar_tarjeta()
+        elif opcion == 3:
+            pass
+        elif opcion == 4:
+            pass
+        elif opcion == 5:
+            pass
+        elif opcion == 6:
+            pass
+        elif opcion == 7:
+            pass
+        elif opcion == 8:
+            pass
+        elif opcion == 9:
+            print("SALIENDO...")
+            exit()
+
+def buscar_tarjeta_usuario():
+    os.system("cls")
+    print("*** BÚSQUEDA DE TARJETA DE USUARIO ***\n")
+    try:
+        nombre_usuario = input("Ingrese el nombre de usuario: ")
+        if nombre_usuario in datos_usuario:
+            tarjeta = datos_usuario[nombre_usuario]
+            print("Tarjeta encontrada:")
+            print(f"Usuario: {nombre_usuario}")
+            print(f"Número de tarjeta: {tarjeta['numero']}")
+            print(f"Saldo: {tarjeta['saldo']}")
+        else:
+            print("No se encontró la tarjeta de usuario.")
+    except KeyError:
+        print("Error: Nombre de usuario inválido.")
 
 def comprar_tarjeta():
-    system("cls")
-    print("*** COMPRA DE TARJETA ***")
+    os.system("cls")
+    print("* COMPRA DE TARJETA *")
     print("                         ")
     print("")
 
-main_menu()
+def recargar_tarjeta():
+    os.system("cls")
+    print("*** RECARGA DE TARJETA ***")
+    print("                         ")
+    valor = float(input("Ingrese cuánto desea recargar: "))
+    tarjeta = buscar_tarjeta_usuario()
+    saldo_actual = tarjeta.get("saldo", 0)
+    nuevo_saldo = saldo_actual + valor
+    tarjeta["saldo"] = nuevo_saldo
+    print(f"¡Tarjeta recargada con éxito! Valor: {valor}")
+    print(f"Nuevo saldo: {nuevo_saldo}")
+
